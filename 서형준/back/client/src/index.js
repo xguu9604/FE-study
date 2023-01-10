1663a5a7bacfd8125899fe2f0dd873cd031851bb
xguu9604 <xguu9604@gmail.com>
2023-01-10 22:27:27 +0900

    finished

--- 서형준/back/client/src/index.js
+++ 서형준/back/client/src/index.js
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+// import 'antd/dist/antd.css'
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise'
+import ReduxThunk from 'redux-thunk'
+import Reducer from './_reducers'
+
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider
+        store={createStoreWithMiddleware(Reducer,
+                window.__REDUX_DEVTOOLS_EXTENSION__&&
+                window.__REDUX_DEVTOOLS_EXTENSION__()
+            )}
+    >
+        <App />
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
